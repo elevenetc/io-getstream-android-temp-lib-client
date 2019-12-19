c8800a9fd42356b31abcb2badd7fdee3bd16b79a
Eugene Levenetc <shumvlesu@gmail.com>
2019-12-19 15:24:55 +0100

    Add retrofit call

--- client/src/main/java/io/getstream/android/temp/lib/client/Client.kt
+++ client/src/main/java/io/getstream/android/temp/lib/client/Client.kt
@@ -1,10 +1,48 @@
 package io.getstream.android.temp.lib.client
 
+import okhttp3.Request
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 class Client {
     suspend fun load(): String {
         val before = System.currentTimeMillis()
         Thread.sleep(1000)
         val after = System.currentTimeMillis()
         return "$before -> $after"
+    }
+
+    fun loadCall(): Call<String> {
+        return object : Call<String> {
+            override fun enqueue(callback: Callback<String>) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun isExecuted(): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun clone(): Call<String> {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun isCanceled(): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun cancel() {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun execute(): Response<String> {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun request(): Request {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        }
     }
 }
